6a13677ad37957e4120f8c1f1d0e013bbd0d8838
tigoarrudawr <tigoarrudawr@gmail.com>
2025-08-26 20:18:10 -0300

    changing scripts

--- set_git_user.sh
+++ set_git_user.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-https://github.com/tigoarrudawr/pw1
+# https://github.com/tigoarrudawr/pw1
 
 git config --global user.email "tigoarrudawr@gmail.com"
 git config --global user.name "tigoarrudawr"
--- unset.sh
+++ unset.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-https://github.com/tigoarrudawr/pw1
+# https://github.com/tigoarrudawr/pw1
 
 git config --global --unset user.email
 git config --global --unset user.name
+
+rm -rf ../pw1
