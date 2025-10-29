0411a30b9d17f32d0fff216fb04fed2ffe15ee0f
tigoarrudawr <tigoarrudawr@gmail.com>
2025-10-28 22:25:10 -0300

    removing scripts imported as scripts and handling the promises

--- script/index.js
+++ script/index.js
@@ -15,31 +15,11 @@ const loremIpsumSettings = {
 }
 
 function preencherSelectMarcas() {
-	loremIpsunGenerator();
-	const selectElement = document.getElementById('marca-select');
-	const optionsHTML = montadoras.sort((a, b) => {
-		if (a.text < b.text) {
-			return -1;
-		}
-		if (a.text > b.text) {
-			return 1;
-		}
-		return 0;
-	}).map(marca => {
-		return `<option value="${marca.value}">${marca.text}</option>`;
-	}).join('');
-	selectElement.innerHTML += '<option value="" selected>Selecione uma marca</option>' + optionsHTML;
-}
-
-function preencherSelectModels() {
-	document.getElementById("marca-select").addEventListener("change", function () {
-		const montadora = this.value;
-		const modeloSelect = document.getElementById("modelo-select");
-
-		modeloSelect.innerHTML = '<option value="" selected>Selecione um modelo</option>';
-
-		if (montadora && modelos[montadora]) {
-			modelos[montadora].sort((a, b) => {
+	fetch("/pw1/data/marcas.json").then(file => {
+		file.text().then(dataString => {
+			let dataObject = JSON.parse(dataString);
+			const selectElement = document.getElementById('marca-select');
+			const optionsHTML = dataObject.sort((a, b) => {
 				if (a.text < b.text) {
 					return -1;
 				}
@@ -47,41 +27,75 @@ function preencherSelectModels() {
 					return 1;
 				}
 				return 0;
-			}).forEach(function (modelo) {
-				const option = document.createElement("option");
-				option.value = modelo.value;
-				option.textContent = modelo.text;
-				modeloSelect.appendChild(option);
+			}).map(marca => {
+				return `<option value="${marca.value}">${marca.text}</option>`;
+			}).join('');
+			selectElement.innerHTML += '<option value="" selected>Selecione uma marca</option>' + optionsHTML;
+		});
+	});
+}
+
+function preencherSelectModels() {
+	fetch("/pw1/data/modelos.json").then(file => {
+		file.text().then(dataString => {
+			let dataObject = JSON.parse(dataString);
+			document.getElementById("marca-select").addEventListener("change", function () {
+				const montadora = this.value;
+				const modeloSelect = document.getElementById("modelo-select");
+
+				modeloSelect.innerHTML = '<option value="" selected>Selecione um modelo</option>';
+
+				if (montadora && dataObject[montadora]) {
+					dataObject[montadora].sort((a, b) => {
+						if (a.text < b.text) {
+							return -1;
+						}
+						if (a.text > b.text) {
+							return 1;
+						}
+						return 0;
+					}).forEach(function (modelo) {
+						const option = document.createElement("option");
+						option.value = modelo.value;
+						option.textContent = modelo.text;
+						modeloSelect.appendChild(option);
+					});
+				}
 			});
-		}
+		});
 	});
 }
 
 function loremIpsunGenerator() {
-	const paragraphLenght = randomNumber(loremIpsumSettings.paragraph);
-	var paragraphList = [];
-	for (var i = 0; i < paragraphLenght; i++) {
-		const sentenceLength = randomNumber(loremIpsumSettings.phrases);
-		var sentenceList = [];
-		for (var j = 0; j < sentenceLength; j++) {
-			const wordsLength = randomNumber(loremIpsumSettings.words);
-			var wordList = [];
-			for (var k = 0; k < wordsLength; k++) {
-				var word = dictionary[Math.floor(Math.random() * dictionary.length)];
-				if (k == 0) {
-					word = word.charAt(0).toUpperCase() + word.slice(1);
-				} else if (k == wordsLength - 1) {
-					word += ".";
-				} else if(Math.random() < loremIpsumSettings.commaChance) {
-					word += ",";
+	return fetch("/pw1/data/lorem_ipsum_dictionary.json").then(file => {
+		return file.text().then(dataString => {
+			let dataObject = JSON.parse(dataString);
+			const paragraphLenght = randomNumber(loremIpsumSettings.paragraph);
+			var paragraphList = [];
+			for (var i = 0; i < paragraphLenght; i++) {
+				const sentenceLength = randomNumber(loremIpsumSettings.phrases);
+				var sentenceList = [];
+				for (var j = 0; j < sentenceLength; j++) {
+					const wordsLength = randomNumber(loremIpsumSettings.words);
+					var wordList = [];
+					for (var k = 0; k < wordsLength; k++) {
+						var word = dataObject[Math.floor(Math.random() * dataObject.length)];
+						if (k == 0) {
+							word = word.charAt(0).toUpperCase() + word.slice(1);
+						} else if (k == wordsLength - 1) {
+							word += ".";
+						} else if (Math.random() < loremIpsumSettings.commaChance) {
+							word += ",";
+						}
+						wordList.push(word);
+					}
+					sentenceList.push(wordList.join(" "));
 				}
-				wordList.push(word);
+				paragraphList.push("<p class='about-us'>" + sentenceList.join(" ") + "</p>");
 			}
-			sentenceList.push(wordList.join(" "));
-		}
-		paragraphList.push("<p class='about-us'>" + sentenceList.join(" ") + "</p>");
-	}
-	return paragraphList.join("\n");
+			return paragraphList.join("\n");
+		});
+	});
 }
 
 function randomNumber(structure) {
@@ -89,29 +103,33 @@ function randomNumber(structure) {
 }
 
 function loadAboutUs() {
-	const loremText = loremIpsunGenerator();
-	// 2. Find the element by its ID
+	const loremText = loremIpsunGenerator().then(loremText => {
+	console.log(loremText);
 	const targetElement = document.getElementById('about-us');
-
 	// 3. Insert the string into the element
-	targetElement.innerHTML = loremText; 
+	targetElement.innerHTML = loremText;
+	});
+	console.log(typeof loremText);
+	
+	// 2. Find the element by its ID
+	
 }
 
 function loadComponent(url, elementId) {
-    fetch(url)
-        .then(response => {
-            // Verifica se a requisição foi bem-sucedida
-            if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
-            }
-            return response.text();
-        })
-        .then(data => {
+	fetch(url)
+		.then(response => {
+			// Verifica se a requisição foi bem-sucedida
+			if (!response.ok) {
+				throw new Error(`HTTP error! status: ${response.status}`);
+			}
+			return response.text();
+		})
+		.then(data => {
 			document.getElementsByTagName(elementId)[0].innerHTML = data;
-        })
-        .catch(e => {
-            console.error(`Erro ao carregar o componente ${url}:`, e);
-        });
+		})
+		.catch(e => {
+			console.error(`Erro ao carregar o componente ${url}:`, e);
+		});
 }
 
 document.addEventListener('DOMContentLoaded', loadComponent('/pw1/pages/footer/footer.html', 'footer-placeholder'));
