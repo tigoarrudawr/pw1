1f9088b48601eaccad782354793758e6dc5f84d3
tigoarrudawr <tigoarrudawr@gmail.com>
2025-11-25 19:56:54 -0300

    adding car pages

---
+++ pages/carros/carros.js
@@ -0,0 +1,24 @@
+function loadComponent(url, elementId) {
+    fetch(url)
+        .then(response => {
+            // Verifica se a requisição foi bem-sucedida
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            return response.text();
+        })
+        .then(data => {
+            document.getElementsByTagName(elementId)[0].innerHTML = data;
+        })
+        .catch(e => {
+            console.error(`Erro ao carregar o componente ${url}:`, e);
+        });
+}
+
+document.addEventListener('DOMContentLoaded', loadComponent('/pw1/pages/footer/footer.html', 'footer-placeholder'));
+document.addEventListener('DOMContentLoaded', loadComponent('/pw1/pages/header/header.html', 'header-placeholder'));
+document.getElementById('contact-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    alert('Thank you for your message! We will get back to you soon.');
+    this.reset();
+});
